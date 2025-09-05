4b587880824f869bb336c8c313b69b043b3e40b1
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-05 19:49:35 +0100

    حدثت دالة إضافة كتاب

--- book.c
+++ book.c
@@ -1,11 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <book.h>
 
 
 //دالة إضافة كتاب
 int addBook(book book) {
-    printf("=========================\n        ADD BOOK\n=========================");
+    char answer;
+    printf("=========================\n        ADD BOOK\n=========================\n");
     printf("Enter name of the book : "); // إدخال اسم الكتاب
     fgets(book.name , MAX_LETTERS , stdin);
+    book.name[strcspn(book.name , "\n")] = '\0';
+    printf("Enter type of the book :");
+    fgets(book.type , 64 , stdin);
+    book.type[strcspn(book.type , "\n")] = '\0';
+    printf("Enter number of page in the book : ");
+    scanf("%d" , &book.pages);
+    getchar();
+    printf("Enter name of writer : ");
+    fgets(book.writer , 64 , stdin);
+    book.writer[strcspn(book.writer , "\n")] = '\0';
+    printf("Enter the date of writing of the book : ");
+    fgets(book.date , 24 , stdin);
+    book.date[strcspn(book.date , "\n")] = '\0';
+    printf("Do you want to creat a description for the book (y for yes and n for no)?\n");
+    scanf("%c" , &answer);
+    getchar();
+    if (answer == 'y') {
+        printf("Enter the description of book : ");
+        fgets(book.description , BOOK_DESCRIPTION , stdin);
+        book.description[strcspn(book.description , "\n")] = '\0';
+        printf("The book is succesfule added.");
+    } else if (answer == 'n') {
+        printf("The book is succesfule added.");
+    } else {
+        printf("The answer don't existe.");
+    }
+
+    return 1;
 }
--- book.h
+++ book.h
@@ -4,8 +4,8 @@
 typedef struct book {
     char name[MAX_LETTERS];
     int pages;
-    char date[12];
+    char date[24];
     char writer[64];
-    char type[32];
+    char type[64];
     char description[BOOK_DESCRIPTION];
 }book;
